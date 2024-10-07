47e0778a0791d083a5eb3e96bce3dc25859f7e27
supermomo668 <mymomo119966.mm@gmail.com>
2024-10-07 22:25:40 +0000

    added models

--- TRAIN.py
+++ TRAIN.py
@@ -8,28 +8,35 @@
 # Labpics Dataset can be downloaded from: https://zenodo.org/records/3697452/files/LabPicsV1.zip?download=1
 # Pretrained models for sam2 Can be downloaded from: https://github.com/facebookresearch/segment-anything-2?tab=readme-ov-file#download-checkpoints
 
+import os
+import tqdm
+import datetime
+
 import numpy as np
 import torch
 import cv2
-import os
+
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 
-# Read data
+# configuration
+sam2_checkpoint = "checkpoints/sam2_hiera_small.pt" # path to model weight (pre model loaded from: https://dl.fbaipublicfiles.com/segment_anything_2/072824/sam2_hiera_small.pt)
+model_cfg = "sam2_hiera_s.yaml" #  model config
+data_dir=r"data/LabPicsV1/Simple/" # Path to dataset (LabPics 1)
+os.mkdir("model/")
+saved_model = f"checkpoints/model/sam2_hiera_small_{datetime.datetime.now().strftime('%Y-%m-%d')_"simple"}.torch"
 
-data_dir=r"data/LabPicsV1//" # Path to dataset (LabPics 1)
+# Read data
 data=[] # list of files in dataset
-for ff, name in enumerate(os.listdir(data_dir+"Simple/Train/Image/")):  # go over all folder annotation
+for ff, name in tqdm(enumerate(os.listdir(data_dir+"Train/Image/")), desc="Reading data"):  # go over all folder annotation
     data.append({
-         "image":data_dir+"Simple/Train/Image/"+name,
-         "annotation":data_dir+"Simple/Train/Instance/"+name[:-4]+".png"
+         "image":data_dir+"Train/Image/"+name,
+         "annotation":data_dir+"Train/Instance/"+name[:-4]+".png"
      })
 
 
 def read_batch(data): # read random image and its annotaion from  the dataset (LabPics)
-
    #  select image
-
         ent  = data[np.random.randint(len(data))] # choose random entry
         Img = cv2.imread(ent["image"])[...,::-1]  # read image
         ann_map = cv2.imread(ent["annotation"]) # read annotation
@@ -60,10 +67,9 @@ def read_batch(data): # read random image and its annotaion from  the dataset (L
         return Img,np.array(masks),np.array(points), np.ones([len(masks),1])
 
 # Load model
-
-sam2_checkpoint = "sam2_hiera_small.pt" # path to model weight (pre model loaded from: https://dl.fbaipublicfiles.com/segment_anything_2/072824/sam2_hiera_small.pt)
-model_cfg = "sam2_hiera_s.yaml" #  model config
-sam2_model = build_sam2(model_cfg, sam2_checkpoint, device="cuda") # load model
+print("Load model...")
+sam2_model = build_sam2(
+     model_cfg, sam2_checkpoint, device="cuda") # load model
 predictor = SAM2ImagePredictor(sam2_model)
 
 # Set training parameters
@@ -80,48 +86,48 @@ scaler = torch.cuda.amp.GradScaler() # mixed precision
 
 # Training loop
 
-for itr in range(100000):
+for itr in tqdm(range(100000), desc="training"):
     with torch.cuda.amp.autocast(): # cast to mix precision
-            image,mask,input_point, input_label = read_batch(data) # load data batch
-            if mask.shape[0]==0: continue # ignore empty batches
-            predictor.set_image(image) # apply SAM image encoder to the image
+          image,mask,input_point, input_label = read_batch(data) # load data batch
+          if mask.shape[0]==0: continue # ignore empty batches
+          predictor.set_image(image) # apply SAM image encoder to the image
 
-            # prompt encoding
+          # prompt encoding
 
-            mask_input, unnorm_coords, labels, unnorm_box = predictor._prep_prompts(input_point, input_label, box=None, mask_logits=None, normalize_coords=True)
-            sparse_embeddings, dense_embeddings = predictor.model.sam_prompt_encoder(points=(unnorm_coords, labels),boxes=None,masks=None,)
+          mask_input, unnorm_coords, labels, unnorm_box = predictor._prep_prompts(input_point, input_label, box=None, mask_logits=None, normalize_coords=True)
+          sparse_embeddings, dense_embeddings = predictor.model.sam_prompt_encoder(points=(unnorm_coords, labels),boxes=None,masks=None,)
 
-            # mask decoder
+          # mask decoder
 
-            batched_mode = unnorm_coords.shape[0] > 1 # multi object prediction
-            high_res_features = [feat_level[-1].unsqueeze(0) for feat_level in predictor._features["high_res_feats"]]
-            low_res_masks, prd_scores, _, _ = predictor.model.sam_mask_decoder(image_embeddings=predictor._features["image_embed"][-1].unsqueeze(0),image_pe=predictor.model.sam_prompt_encoder.get_dense_pe(),sparse_prompt_embeddings=sparse_embeddings,dense_prompt_embeddings=dense_embeddings,multimask_output=True,repeat_image=batched_mode,high_res_features=high_res_features,)
-            prd_masks = predictor._transforms.postprocess_masks(low_res_masks, predictor._orig_hw[-1])# Upscale the masks to the original image resolution
+          batched_mode = unnorm_coords.shape[0] > 1 # multi object prediction
+          high_res_features = [feat_level[-1].unsqueeze(0) for feat_level in predictor._features["high_res_feats"]]
+          low_res_masks, prd_scores, _, _ = predictor.model.sam_mask_decoder(image_embeddings=predictor._features["image_embed"][-1].unsqueeze(0),image_pe=predictor.model.sam_prompt_encoder.get_dense_pe(),sparse_prompt_embeddings=sparse_embeddings,dense_prompt_embeddings=dense_embeddings,multimask_output=True,repeat_image=batched_mode,high_res_features=high_res_features,)
+          prd_masks = predictor._transforms.postprocess_masks(low_res_masks, predictor._orig_hw[-1])# Upscale the masks to the original image resolution
 
-            # Segmentaion Loss caclulation
+          # Segmentaion Loss caclulation
 
-            gt_mask = torch.tensor(mask.astype(np.float32)).cuda()
-            prd_mask = torch.sigmoid(prd_masks[:, 0])# Turn logit map to probability map
-            seg_loss = (-gt_mask * torch.log(prd_mask + 0.00001) - (1 - gt_mask) * torch.log((1 - prd_mask) + 0.00001)).mean() # cross entropy loss
+          gt_mask = torch.tensor(mask.astype(np.float32)).cuda()
+          prd_mask = torch.sigmoid(prd_masks[:, 0])# Turn logit map to probability map
+          seg_loss = (-gt_mask * torch.log(prd_mask + 0.00001) - (1 - gt_mask) * torch.log((1 - prd_mask) + 0.00001)).mean() # cross entropy loss
 
-            # Score loss calculation (intersection over union) IOU
+          # Score loss calculation (intersection over union) IOU
 
-            inter = (gt_mask * (prd_mask > 0.5)).sum(1).sum(1)
-            iou = inter / (gt_mask.sum(1).sum(1) + (prd_mask > 0.5).sum(1).sum(1) - inter)
-            score_loss = torch.abs(prd_scores[:, 0] - iou).mean()
-            loss=seg_loss+score_loss*0.05  # mix losses
+          inter = (gt_mask * (prd_mask > 0.5)).sum(1).sum(1)
+          iou = inter / (gt_mask.sum(1).sum(1) + (prd_mask > 0.5).sum(1).sum(1) - inter)
+          score_loss = torch.abs(prd_scores[:, 0] - iou).mean()
+          loss=seg_loss+score_loss*0.05  # mix losses
 
-            # apply back propogation
+          # apply back propogation
 
-            predictor.model.zero_grad() # empty gradient
-            scaler.scale(loss).backward()  # Backpropogate
-            scaler.step(optimizer)
-            scaler.update() # Mix precision
+          predictor.model.zero_grad() # empty gradient
+          scaler.scale(loss).backward()  # Backpropogate
+          scaler.step(optimizer)
+          scaler.update() # Mix precision
 
-            if itr%1000==0: torch.save(predictor.model.state_dict(), "model.torch");print("save model")
+          if itr%1000==0: torch.save(predictor.model.state_dict(), saved_model);print("save model")
 
-            # Display results
+          # Display results
 
-            if itr==0: mean_iou=0
-            mean_iou = mean_iou * 0.99 + 0.01 * np.mean(iou.cpu().detach().numpy())
-            print("step)",itr, "Accuracy(IOU)=",mean_iou)
+          if itr==0: mean_iou=0
+          mean_iou = mean_iou * 0.99 + 0.01 * np.mean(iou.cpu().detach().numpy())
+          print("step)",itr, "Accuracy(IOU)=",mean_iou)
--- docs/DEPENDENCY_SETUP.md
+++ docs/DEPENDENCY_SETUP.md
@@ -5,5 +5,10 @@ apt install nvidia-cuda-toolkit nvidia-cuda-toolkit-gcc
 ```
 ## Dependencies set-up
 ```
+# this method face issue when running the program from poetry shell
 poetry install
+```
+currently recommend:
+```
+pip install -e .
 ```
